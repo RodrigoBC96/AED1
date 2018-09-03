47808b6f02b3c80098f865f27d7161aef0c8ea4b
RodrigoBC96 <33148165+RodrigoBC96@users.noreply.github.com>
2018-09-03 16:08:30 -0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct Agenda{
+    char nome[30];
+    int idade;
+}
+typedef struct Agenda Agenda;
+
+void Insert(); //To Do
+void Remove(); //To Do
+void List(); //To Do
+
+int main(){ //On Going
+
+void *pBuffer;
+pBuffer = malloc(2);
+
+free(pBuffer);
+return 0;
+}
