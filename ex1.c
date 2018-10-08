0303d039243b8644e9b1c47d1b8bad0bb8cdf333
RodrigoBC96 <33148165+RodrigoBC96@users.noreply.github.com>
2018-10-08 16:57:22 -0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -8,36 +8,64 @@ struct Agenda{
 typedef struct Agenda Agenda;
 
 //Inserção de pessoas na agenda
-void Insert(void *pBuffer){
-} //To Do
+void *Insert(void *pBuffer, int *pTam){
+    *pBuffer = realloc(pBuffer,3*sizeof(int)+((*pTam+1)*sizeof(Agenda)));
+    void *aux = pBuffer+3*sizeof(int)+*pTam*sizeof(Agenda);
+    Agenda *new;
+    new = malloc(sizeof(Agenda));
+    printf("Insira o nome da pessoa:\n");
+    scanf("%[^\n]s",*new->nome);
+    printf("Insira a idade da pessoa:\n");
+    scanf("%d",*new->idade);
+    *aux = new;
+    *pTam++;
+    return pBuffer;
+}//Ready
 
 //Ordenação da lista por meio do método Insertion Sort
-void Insertion(void *pBuffer, int *pTam){
+void *Insertion(void *pBuffer, int *pTam){
 }//To Do
 
 //Ordenação da lista por meio do método Selection Sort
-void Select(void *pBuffer, int *pTam){
+void *Select(void *pBuffer, int *pTam){
 } //To Do
 
 //Ordenação da lista por meio do método Bubble Sort
-void Bubble(void *pBuffer, int *pTam){
+void *Bubble(void *pBuffer, int *pTam){
 } // To Do
 
+//Ordenação da lista por meio do método Quick Sort
+void *Quick(void *pBuffer, int *pTam){
+}//To Do
+
+//Ordenação da lista por meio do método Merge Sort
+void *Merge(void *pBuffer, int *pTam){
+}//To Do
+
 //Remoção de um membro(instância) da agenda
-void Remove(void *pBuffer, int *pTam){
+void *Remove(void *pBuffer, int *pTam){
 } //To Do
 
 //Listagem de todos os membros da agenda
-void List(void *pBuffer, int *pTam){
-} //To Do
+void List(void *pBuffer, int *pTam, int *cont){
+    Agenda *aux = pBuffer+3*sizeof(int);
+    for(*cont = 0; *cont < *pTam; *cont++){
+        printf("Nome: %s\n",*aux->nome);
+        printf("Idade: %d\n\n",*aux->idade);
+        aux++;
+    }
+}//Ready
 
-int main(){ //On Going
+int main(){ //Ready
 
 void *pBuffer;
-int *pOp, *pTam;//May have more
+int *pOp, *pTam, *cont;
     
-//Alocação de memória inicial
-    
+pBuffer = malloc(3*sizeof(int));
+pOp = pBuffer;
+pTam = pBuffer+sizeof(int);
+cont = pBuffer+2*sizeof(int);
+   
 do{
     printf("Escolha a opção:\n");
     printf("1.Inserir Elementos;\n");
@@ -51,19 +79,19 @@ do{
     scanf("%d",pOp);    
       switch(*pOp){
           case 1:
-                Insert(pBuffer,pTam);
+                pBuffer = Insert(pBuffer,pTam);
                 break;
           case 2:
-                Insertion(pBuffer,pTam);
+                pBuffer = Insertion(pBuffer,pTam);
                 break;
           case 3:
-                Select(pBuffer,pTam);
+                pBuffer = Select(pBuffer,pTam);
                 break;
           case 4:
-                Bubble(pBuffer,pTam);
+                pBuffer = Bubble(pBuffer,pTam);
                 break;
           case 5:
-                Remove(pBuffer,pTam);
+                pBuffer = Remove(pBuffer,pTam);
                 break;
           case 6:
                 List(pBuffer,pTam);
